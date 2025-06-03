2a3001100dcef04b9d18f111795e03028001a4ee
gretamagkekse <gretamoeller99@gmail.com>
2025-06-03 13:48:59 +0200

    seperate files for cleaner code

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import logging
+
+SENSOR_IDS = ["sensor1", "sensor2", "sensor3", "sensor4", "sensor5"]
+KEY_DIR = "./keys"
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("sensor-data-service")
+
---
+++ src/models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Distance(BaseModel):
+    actual: Optional[float] = None
+    gcd: Optional[float] = None
+    sfd: Optional[float] = None
+
+class SensorData(BaseModel):
+    distance: Distance
+
+class TceSensorData(BaseModel):
+    tceId: str
+    camundaProcessInstanceKey: str
+    camundaActivityId: str
+    sensorkey: str
+    signedSensorData: str
+    sensorData: SensorData
+
+class SensorRequest(BaseModel):
+    shipment_id: str
+    tceId: str
+    camundaProcessInstanceKey: str
+    camundaActivityId: str
